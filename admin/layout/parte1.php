bb92a10b95f17c4003d58785f989ae03aa78c56f
BautistaTordini <bautistatordini.uns@gmail.com>
2024-12-13 20:37:04 -0300

    chore: Refactor login controller to use session for success and failure messages and redirect

---
+++ admin/layout/parte1.php
@@ -0,0 +1,139 @@
+<?php 
+
+session_start();
+
+if (isset($_SESSION['session_email'])) {
+    // echo "paso por el login";
+    $email_session = $_SESSION['session_email'];
+
+    $sql = "SELECT * FROM usuarios WHERE email = '$email_session'";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+
+    $usuarios = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($usuarios as $usuario) {
+        $id_session_usuario = $usuario['id_usuario'];
+    }
+} else{
+    // redireccion $APP_URL = ./login
+    header('Location: '.APP_URL.'/login');
+}
+
+?>
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?=APP_NAME;?></title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="<?=APP_URL;?>/public/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?=APP_URL;?>/public/dist/css/adminlte.min.css">
+  <!-- IconBootstrap -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="<?=APP_URL;?>/admin" class="nav-link"><?=APP_NAME;?></a>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li>
+      <!-- <li class="nav-item">
+        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+          <i class="fas fa-th-large"></i>
+        </a>
+      </li> -->
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="<?=APP_URL;?>/admin" class="brand-link">
+        <video class="brand-image img-circle elevation-3" autoplay loop muted>
+            <source src="https://cdn-icons-mp4.flaticon.com/512/11933/11933440.mp4" type="video/mp4">
+            Tu navegador no soporta el formato de video.
+        </video>
+    
+      <span class="brand-text font-weight-light">SIS DELIVERY</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="info">
+          <a href="#" class="d-block"><?=$email_session;?></a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- ROLES -->
+
+          <li class="nav-item">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon bi bi-person-rolodex"></i>
+              <p>
+                Roles
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="<?=APP_URL;?>/admin/roles/create-rol.php" class="nav-link">
+                  <i class="bi bi-journal-plus nav-icon"></i>
+                  <p>Crear Rol</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="<?=APP_URL;?>/admin/roles" class="nav-link">
+                  <i class="bi bi-person-lines-fill nav-icon"></i>
+                  <p>Listado de roles</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="<?=APP_URL;?>/login/logout.php" class="nav-link" style="background-color: #a9200e; color:white;">
+            <i class="nav-icon bi bi-box-arrow-left"></i>
+              <p>
+                Cerrar Sesion
+              </p>
+            </a>
+          </li>
+          
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
---
+++ admin/roles/index.php
@@ -0,0 +1,26 @@
+<?php
+include('../../app/config.php'); //incluir archivo de configuración
+
+include('../../admin/layout/parte1.php'); //cargar la primera parte del layout
+
+include('../../app/roles/listado-de-roles.php'); //cargar el listado de roles
+
+?>
+<div class="content-wrapper">
+  <br>
+  <!-- main content -->
+  <div class="content">
+    <div class="container-fluid"> 
+      <div class="container">
+        <h1>Listado De Roles</h1>
+      </div>
+    </div>
+  </div>
+  <!-- /.content -->
+</div>
+
+<?php 
+
+include('../../admin/layout/parte2.php'); //cargar la segunda parte del layout
+
+?>
